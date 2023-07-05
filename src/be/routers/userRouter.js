46d1d02d3d696c28938bf58af88cebe9d7d9602e
Hyeongyu-619 <110807563+Hyeongyu-619@users.noreply.github.com>
2023-07-05 12:02:50 +0900

    feat: 폴더 구조 변경

---
+++ src/be/routers/userRouter.js
@@ -0,0 +1,20 @@
+import express from 'express'; 
+import User from '../models/userModel';
+
+const userRouter = express.Router();
+
+userRouter.get("/createadmin", async(req, res) => {
+    try {
+        const user = new User({
+            name: 'admin',
+            email: 'blogtest@gmail.com',
+            password: 'blogtest@',
+        });
+        const createUser = await user.save();
+        res.send(createUser);
+    } catch(err){
+        res.status(500).send({ message: err.message })
+    }
+});
+
+export default userRouter;
