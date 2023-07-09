3a47d85dab73debc57d14db8435b9041d48c1cb6
Hyeongyu-619 <110807563+Hyeongyu-619@users.noreply.github.com>
2023-07-09 21:31:35 +0900

    Feat: viewsRouter 추가

---
+++ src/be/routers/viewRouter.js
@@ -0,0 +1,24 @@
+import express from "express";
+import path from "path";
+import url from "url";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const viewsRouter = express.Router();
+
+viewsRouter.use("/", serveStatic("home"));
+viewsRouter.use("/register", serveStatic("register"));
+viewsRouter.use("/login", serveStatic("login"));
+viewsRouter.use("/productList", serverStatic("productList"));
+viewsRouter.use("/productsDetail", serverStatic("productDetail"));
+viewsRouter.use("/", serveStatic(""));
+
+function serveStatic(resource) {
+  const resourcePath = path.join(__dirname, `../fe/views/${resource}`);
+  const option = { index: `${resource}.html` };
+
+  return express.static(resourcePath, option);
+}
+
+export { viewsRouter };
