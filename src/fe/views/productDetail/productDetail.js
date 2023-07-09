7cad62df9ed9f5eff9d6242c8ca07598240a87fa
joayeon <codinz.n@gmail.com>
2023-07-10 06:43:36 +0900

    Comment: productDetail 주석 삭제

--- src/fe/views/productDetail/productDetail.js
+++ src/fe/views/productDetail/productDetail.js
@@ -1,14 +1,8 @@
-/*
-백엔드에서 받아와서 상품 상세 페이지에 필요한 데이터
-
-상품 객체 { 상품 번호: number, 상품 종류: string, 등록 시간: date, 상품 이미지: string, 상품 명: string, 상품 가격: price }
-
-장바구니에 담았을 때 넘겨줘야 하는 정보 ↓
-상품 객체 { 상품 번호: number, 상품 종류: string, 등록 시간: date, 상품 이미지: string, 상품 명: string, 상품 가격: price*quantity, 상품 갯수: number }
-*/
-
 // 상품 상세 페이지 리스트 업
 async function init() {
+    // 상품 번호 가져오기
+    let productNum = {};
+
     const response = await fetch('../../public/db/productDummy.json');
     const data = await response.json();
     const productDummy = data.product;
