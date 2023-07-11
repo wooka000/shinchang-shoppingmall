ece9528c5c2e9b53efb5f2c6a10fcbca19f7f8f7
l22coder <l22coder@gmail.com>
2023-07-12 02:00:03 +0900

    Fix: productsSchema 등록시간 현재시간으로 수정, orderRouter 불필요 주석삭제, orderSchema 불필요 내용 삭제

--- src/be/database/schema/orderSchema.js
+++ src/be/database/schema/orderSchema.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 
 const OrderSchema = new Schema({
   orderNo: { type: Number, required: true }, // 주문번호
-  orderer: { type: String, ref: "User", required: true }, // 주문자
+  orderer: { type: String, required: true }, // 주문자
   orderProduct: { type: String, required: true }, // 주문상품
   price: { type: Number, required: true }, // 가격
   orderStatus: { type: String, required: true }, // 주문상태
