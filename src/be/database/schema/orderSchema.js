5864bb2211b2646566239daa56d15f2f4e2c77db
l22coder <l22coder@gmail.com>
2023-07-12 13:00:20 +0900

    Fix: order스키마 내용 수정

--- src/be/database/schema/orderSchema.js
+++ src/be/database/schema/orderSchema.js
@@ -3,11 +3,22 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const OrderSchema = new Schema({
-  orderNo: { type: Number, required: true }, // 주문번호
-  orderer: { type: String, required: true }, // 주문자
-  orderProduct: { type: String, required: true }, // 주문상품
-  price: { type: Number, required: true }, // 가격
-  orderStatus: { type: String, required: true }, // 주문상태
+  orderNo: { type: Number, required: true, index: true }, // 주문 번호
+  userName: { type: String, required: true }, // 주문자 이름
+  phoneNumber: { type: String, required: true }, // 주문자 전화번호
+  userEmail: { type: String, default: "" }, // 주문자 이메일
+  recipientName: { type: String, required: true }, // 수령인 이름
+  addressCode: { type: String, required: true }, // 우편번호
+  address: { type: String, required: true }, // 주소
+  detailAddress: { type: String, default: "" }, // 상세주소
+  extraAddress: { type: String, default: "" }, // 주소 참고항목
+  deliveryMessage: { type: String, default: "" }, // 배송 메모
+  orderArray: [
+    {
+      productNo: { type: Number, required: true }, // 상품 번호
+      quantity: { type: Number, required: true }, // 상품 수량
+    },
+  ],
 });
 
 export { OrderSchema };
