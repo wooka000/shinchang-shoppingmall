e7dcf25646587c28f8634e48cd7847d28a389d5f
joayeon <codinz.n@gmail.com>
2023-07-12 12:45:16 +0900

    Feat: home 상품 하단 구현
    home 상품 하단 카테고리 이름 불러와서 그려주는 부분 구현했습니다.

--- src/fe/views/home/home.js
+++ src/fe/views/home/home.js
@@ -39,6 +39,38 @@ async function newProductRender() {
 
 newProductRender();
 
+async function popularCategoryRender() {
+    const response = await fetch('/api/category', {
+        method: 'GET',
+    });
+    const data = await response.json();
+
+    const productContents = document.querySelector('.popular-contents');
+    const popularCategoryFragment = new DocumentFragment();
+
+    data.forEach(({ categoryName }) => {
+        const div = document.createElement('div');
+        div.setAttribute('class', 'popular-category');
+
+        if (categoryName != '인기 | 신상품') {
+            div.innerHTML = `
+            <div class="category-img-wrapper">
+                <a href="/products/category?categoryName=${categoryName}">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKz_qwtTkmVri-erStn2lSSQ7SRxmC-7pMQw&usqp=CAU" alt="goods 1" />
+                </a>
+            <div>
+            <div class="title">
+                <a href="/products/category?categoryName=${categoryName}"><strong>${categoryName}</strong></a>
+            </div>`;
+            popularCategoryFragment.appendChild(div);
+        }
+    });
+
+    productContents.appendChild(popularCategoryFragment);
+}
+
+popularCategoryRender();
+
 // aos 초기화
 // data-aos="fade-up" data-aos-delay="50" data-aos-duration="1000"
 AOS.init();
