c62bcd53bd43a0964cdaa96473b689eb083df2d3
DESKTOP-QAOAME2\82103 <wlalcn707@naver.com>
2023-07-13 13:24:54 +0900

    기능수정

--- src/fe/views/register/register.js
+++ src/fe/views/register/register.js
@@ -281,6 +281,7 @@ register.addEventListener("click", function (e) {
         })
         .then((responseData) => {
             console.log("회원가입이 성공적으로 완료되었습니다.", responseData);
+            alert("회원가입이 완료되었습니다.로그인 페이지로 이동합니다 ");
             window.location.href = "/login";
         })
         .catch((error) => {
