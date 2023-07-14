9682dc0053ba931ff1676e43e7b1a4f01a4105c6
wooka000 <wooka000@naver.com>
2023-07-14 16:03:21 +0900

    Fix: rebase

--- src/fe/views/login/login.js
+++ src/fe/views/login/login.js
@@ -1,51 +1,51 @@
 function loginId() {
-    let id = document.querySelector("#id");
-    let password = document.querySelector("#password");
+  let id = document.querySelector("#id");
+  let password = document.querySelector("#password");
 
-    if (id.value == "") {
-        alert("아이디를 입력해주세요");
-        return false;
-    }
-    if (password.value == "") {
-        alert("비밀번호를 입력해주세요");
-        return false;
-    }
-    return true;
+  if (id.value == "") {
+    alert("아이디를 입력해주세요");
+    return false;
+  }
+  if (password.value == "") {
+    alert("비밀번호를 입력해주세요");
+    return false;
+  }
+  return true;
 }
 
 // 로그인 버튼 클릭 이벤트 핸들러
 const loginBtn = document.querySelector("#login-btn");
-loginBtn.addEventListener("submit", function (e) {
-    e.preventDefault();
-    console.log(e);
-    // 아이디와 비밀번호 입력 값 가져오기
-    var username = document.getElementById("id").value;
-    var password = document.getElementById("password").value;
+loginBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  // console.log(e);
+  // 아이디와 비밀번호 입력 값 가져오기
+  var username = document.getElementById("id").value;
+  var password = document.getElementById("password").value;
 
-    // API 호출
-    fetch("/user/login", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            email: username,
-            password: password,
-        }),
+  // API 호출
+  fetch("/api/user/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: username,
+      password: password,
+    }),
+  })
+    .then(function (response) {
+      // 응답 상태 코드 확인
+      if (response.status === 200) {
+        // 로그인 성공 시 메인 페이지로 이동
+        window.location.href = "/";
+      } else {
+        // 로그인 실패 처리
+        console.log("로그인 실패");
+      }
     })
-        .then(function (response) {
-            // 응답 상태 코드 확인
-            if (response.status === 200) {
-                // 로그인 성공 시 메인 페이지로 이동
-                window.location.href = "/main";
-            } else {
-                // 로그인 실패 처리
-                console.log("로그인 실패");
-            }
-        })
-        .catch(function (error) {
-            console.log("에러 발생:", error);
-        });
+    .catch(function (error) {
+      console.log("에러 발생:", error);
+    });
 });
 // 아이디 비번 입력한다.
 // 로그인버튼 누른다.
