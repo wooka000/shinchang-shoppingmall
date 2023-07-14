49153b1d6bca2e4154535c349e2dd206fdde887e
DESKTOP-QAOAME2\82103 <wlalcn707@naver.com>
2023-07-15 01:58:27 +0900

    button기능 구현

--- src/fe/views/login/login.js
+++ src/fe/views/login/login.js
@@ -1,63 +1,144 @@
+// function loginId() {
+//   const id = document.querySelector("#id").value;
+//   const password = document.querySelector("#password").value;
+
+//   if (id == "") {
+//     alert("아이디를 입력해 주세요.");
+//     return false;
+//   }
+
+//   if (password == "") {
+//     alert("비밀번호를 입력해 주세요.");
+//     return false;
+//   }
+
+//   return true;
+// }
+
+// // 로그인 버튼 클릭 이벤트 핸들러
+// const loginBtn = document.querySelector("#login-btn");
+// loginBtn.addEventListener("click", (e) => {
+//   e.preventDefault();
+
+//   // 아이디와 비밀번호 입력 값 가져오기
+//   const username = document.getElementById("id").value;
+//   const password = document.getElementById("password").value;
+
+//   // API 호출
+//   fetch("/api/user/login", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       email: username,
+//       password: password,
+//     }),
+//   })
+//     .then((response) => {
+//       if (response.status === 200) {
+//         // 로그인 성공 시 토큰 값을 가져와서 로컬 스토리지에 저장
+//         return response.json().then((data) => {
+//           // 토큰 값 로컬 스토리지에 저장
+//           localStorage.setItem("token", data.token);
+//           localStorage.setItem("username", username);
+
+//           console.log("로그인 성공");
+//         });
+//       } else {
+//         // 로그인 실패 처리
+//         console.log("로그인 실패");
+//         alert("아이디 및 비밀번호를 잘못 입력하였습니다.");
+//         throw new Error("로그인에 실패하였습니다.");
+//       }
+//     })
+//     .then(function () {
+//       // 로그인 성공 후 페이지 이동
+//       window.location.href = "/";
+//     })
+//     .catch(function (error) {
+//       console.log("에러 발생:", error);
+//       // window.location.href = '/error';
+//     });
+// });
+
 function loginId() {
-  const id = document.querySelector("#id").value;
-  const password = document.querySelector("#password").value;
+    let id = document.querySelector("#id");
+    let password = document.querySelector("#password");
 
-  if (id == "") {
-    alert("아이디를 입력해 주세요.");
-    return false;
-  }
-
-  if (password == "") {
-    alert("비밀번호를 입력해 주세요.");
-    return false;
-  }
-
-  return true;
+    if (id.value == "") {
+        alert("아이디를 입력해주세요");
+        return false;
+    }
+    if (password.value == "") {
+        alert("비밀번호를 입력해주세요");
+        return false;
+    }
+    return true;
 }
 
-// 로그인 버튼 클릭 이벤트 핸들러
 const loginBtn = document.querySelector("#login-btn");
-loginBtn.addEventListener("click", (e) => {
-  e.preventDefault();
+loginBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (loginId()) {
+        var username = document.getElementById("id").value;
+        var password = document.getElementById("password").value;
 
-  // 아이디와 비밀번호 입력 값 가져오기
-  const username = document.getElementById("id").value;
-  const password = document.getElementById("password").value;
+        fetch("/api/user/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: username,
+                password: password,
+            }),
+        })
+            .then(function (response) {
+                console.log(response);
+                if (response.status === 200) {
+                    return response.json().then(function (data) {
+                        console.log(data);
+                        localStorage.setItem("token", data.token);
+                        localStorage.setItem("username", username);
+                        console.log("로그인 성공");
+                    });
+                } else {
+                    console.log("로그인 실패");
+                    throw new Error("로그인에 실패하였습니다.");
+                }
+            })
+            .then(function () {
+                window.location.href = "/";
+            })
+            .catch(function (error) {
+                console.log("에러 발생:", error);
+                window.location.href = "/error";
+            });
+    }
+});
 
-  // API 호출
-  fetch("/api/user/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email: username,
-      password: password,
-    }),
-  })
-    .then((response) => {
-      if (response.status === 200) {
-        // 로그인 성공 시 토큰 값을 가져와서 로컬 스토리지에 저장
-        return response.json().then((data) => {
-          // 토큰 값 로컬 스토리지에 저장
-          localStorage.setItem("token", data.token);
-          localStorage.setItem("username", username);
+document.getElementById("id").addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("password").focus();
+    }
+});
 
-          console.log("로그인 성공");
-        });
-      } else {
-        // 로그인 실패 처리
-        console.log("로그인 실패");
-        alert("아이디 및 비밀번호를 잘못 입력하였습니다.");
-        throw new Error("로그인에 실패하였습니다.");
-      }
-    })
-    .then(function () {
-      // 로그인 성공 후 페이지 이동
-      window.location.href = "/";
-    })
-    .catch(function (error) {
-      console.log("에러 발생:", error);
-      // window.location.href = '/error';
-    });
+document.getElementById("password").addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        if (loginId()) {
+            loginBtn.click();
+        }
+    }
+});
+
+window.addEventListener("load", function () {
+    var token = localStorage.getItem("token");
+    if (token) {
+        console.log("토큰 값:", token);
+    } else {
+        console.log("토큰 값이 저장되어 있지 않습니다.");
+    }
 });
--- src/fe/views/register/register.js
+++ src/fe/views/register/register.js
@@ -1,132 +1,145 @@
 function execDaumPostcode() {
     new daum.Postcode({
         oncomplete: function (data) {
-            var addr = ''; // 주소 변수
-            var extraAddr = ''; // 참고항목 변수
+            var addr = ""; // 주소 변수
+            var extraAddr = ""; // 참고항목 변수
 
-            if (data.userSelectedType === 'R') {
+            if (data.userSelectedType === "R") {
                 addr = data.roadAddress;
             } else {
                 addr = data.jibunAddress;
             }
 
-            if (data.userSelectedType === 'R') {
-                if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
+            if (data.userSelectedType === "R") {
+                if (data.bname !== "" && /[동|로|가]$/g.test(data.bname)) {
                     extraAddr += data.bname;
                 }
-                if (data.buildingName !== '' && data.apartment === 'Y') {
-                    extraAddr += extraAddr !== '' ? ', ' + data.buildingName : data.buildingName;
+                if (data.buildingName !== "" && data.apartment === "Y") {
+                    extraAddr +=
+                        extraAddr !== ""
+                            ? ", " + data.buildingName
+                            : data.buildingName;
                 }
-                if (extraAddr !== '') {
-                    extraAddr = ' (' + extraAddr + ')';
+                if (extraAddr !== "") {
+                    extraAddr = " (" + extraAddr + ")";
                 }
-                document.getElementById('sample6_extraAddress').value = extraAddr;
+                document.getElementById("sample6_extraAddress").value =
+                    extraAddr;
             } else {
-                document.getElementById('sample6_extraAddress').value = '';
+                document.getElementById("sample6_extraAddress").value = "";
             }
 
-            document.getElementById('sample6_postcode').value = data.zonecode;
-            document.getElementById('sample6_address').value = addr;
-            document.getElementById('sample6_detailAddress').focus();
+            document.getElementById("sample6_postcode").value = data.zonecode;
+            document.getElementById("sample6_address").value = addr;
+            document.getElementById("sample6_detailAddress").focus();
         },
     }).open();
 }
 
-function formCheck() {
-    const id = document.querySelector('#id').value;
-    const password = document.querySelector('#password').value;
-    const checkPassword = document.querySelector('#passwordc').value;
-    const name = document.querySelector('#name').value;
-    const number = document.querySelector('#number').value;
+// ID 유효성 검사
+function formcheck() {
+    let id = document.querySelector("#id");
+    let password = document.querySelector("#password");
+    let passwordcheck = document.querySelector("#passwordc");
+    let name = document.querySelector("#name");
+    let number = document.querySelector("#number");
 
-    const checkId = /^[a-zA-Z0-9]{4,10}$/;
-    const checkPasswordForm = /^[a-zA-Z0-9]{4,10}$/;
-    const checkNumber = /^\d{3}-\d{3,4}-\d{4}$/;
+    let checkname = /^[가-힣]{4,10}$/;
+    let idcheck = /^[a-zA-Z0-9]{4,10}$/;
+    let passwordcheckk = /^[0-9]+$/;
+    let numbercheck = /^[0-9]+$/;
 
-    if (id === '') {
-        alert('아이디를 입력해 주세요.');
+    if (id.value === "") {
+        alert("아이디를 입력해주세요");
         return false;
     }
-    if (!checkId.test(id)) {
-        alert('영문 및 숫자만 4-10자리까지 입력해 주세요.');
+    if (!idcheck.test(id.value)) {
+        alert("영문 및 숫자만 4-10자리까지 입력해주세요");
         return false;
     }
-    if (password === '' && checkPasswordForm.test(password)) {
-        alert('비밀번호를 입력해 주세요.');
+    if (password.value === "") {
+        alert("비밀번호를 입력해주세요");
         return false;
     }
-    if (checkPassword === '' && checkPasswordForm.test(checkPassword)) {
-        alert('다시 한 번 비밀번호를 입력해 주세요.');
+    if (passwordcheck.value === "") {
+        alert("비밀번호를 입력해주세요");
         return false;
     }
-    if (password !== checkPassword) {
-        alert('비밀번호가 일치하지 않습니다.');
+    if (password.value !== passwordcheck.value) {
+        alert("비밀번호가 일치하지 않습니다");
         return false;
     }
-    if (name === '') {
-        alert('이름을 입력해 주세요.');
+    if (name.value === "") {
+        alert("이름을 입력해주세요");
         return false;
     }
-    if (number === '') {
-        alert('전화번호를 입력해 주세요.');
+    if (number.value === "") {
+        alert("전화번호를 입력해주세요");
         return false;
     }
-    if (!checkNumber.test(number)) {
-        alert('전화번호를 형식에 맞게 입력해 주세요.');
+    if (!numbercheck.test(number.value)) {
+        alert("숫자로만 입력해주세요");
         return false;
     }
-
     return true;
 }
 
-const register = document.querySelector('#register');
-register.addEventListener('click', (e) => {
-    const isValid = formCheck();
+const registerButton = document.querySelector("#register");
+registerButton.addEventListener("click", function (e) {
+    const isValid = formcheck();
 
     if (!isValid) {
         e.preventDefault();
         return;
     }
 
-    const nameInput = document.getElementById('name').value;
-    const emailInput = document.getElementById('id').value;
-    const passwordInput = document.getElementById('password').value;
-    const phoneNumberInput = document.getElementById('number').value;
-    const address1Input = document.getElementById('sample6_address').value;
-    const address2Input = document.getElementById('sample6_detailAddress').value;
-    const postalCodeInput = document.getElementById('sample6_postcode').value;
+    const nameInput = document.getElementById("name");
+    const emailInput = document.getElementById("id");
+    const passwordInput = document.getElementById("password");
+    const phoneNumberInput = document.getElementById("number");
+    const address1Input = document.getElementById("sample6_address");
+    const address2Input = document.getElementById("sample6_detailAddress");
+    const postalCodeInput = document.getElementById("sample6_postcode");
 
     const subscriptionDate = new Date();
 
-    fetch('/api/user/register', {
-        method: 'POST',
+    fetch("/api/user/register", {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            name: nameInput,
-            email: emailInput,
-            password: passwordInput,
-            phoneNumber: phoneNumberInput,
-            address1: address1Input,
-            address2: address2Input,
-            postalCode: postalCodeInput,
+            name: nameInput.value,
+            email: emailInput.value,
+            password: passwordInput.value,
+            phoneNumber: phoneNumberInput.value,
+            address1: address1Input.value,
+            address2: address2Input.value,
+            postalCode: postalCodeInput.value,
             subscriptionDate: subscriptionDate,
         }),
     })
         .then((response) => {
             if (!response.ok) {
-                throw new Error('회원가입에 실패했습니다.');
+                throw new Error("회원가입에 실패했습니다.");
             }
             return response.json();
         })
         .then((responseData) => {
-            console.log('회원가입이 성공적으로 완료되었습니다.', responseData);
-            alert('회원가입이 완료 되었습니다. 로그인 페이지로 이동합니다.');
-            window.location.href = '/login';
+            console.log("회원가입이 성공적으로 완료되었습니다.", responseData);
+            alert("회원가입이 완료 되었습니다. 로그인 페이지로 이동합니다 ");
+            window.location.href = "/login";
         })
         .catch((error) => {
-            console.error('회원가입 중 오류가 발생했습니다.', error);
-            window.location.href = '/error';
+            console.error("회원가입 중 오류가 발생했습니다.", error);
+            window.location.href = "/error";
         });
+});
+
+// Enter 키 누를 때 회원가입 수행
+document.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        registerButton.click();
+    }
 });
