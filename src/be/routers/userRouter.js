a9300f22a18cc2a24c95e605b1ecac89ef17358d
Hyeongyu-619 <110807563+Hyeongyu-619@users.noreply.github.com>
2023-07-14 10:07:36 +0900

    userRouter edit

--- src/be/routers/userRouter.js
+++ src/be/routers/userRouter.js
@@ -62,7 +62,7 @@ userRouter.patch("/my", loginRequired, async function (req, res, next) {
     const userId = req.currentUserId;
 
     // body data 로부터 업데이트할 사용자 정보를 추출함.
-    const fullName = req.body.fullName;
+    const fullName = req.body.name;
     const password = req.body.password;
     const postalCode = req.body.postalCode;
     const address1 = req.body.address1;
